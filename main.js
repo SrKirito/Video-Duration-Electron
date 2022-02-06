01c7357730f4716c26cf4371cc8ec596d32f8bb6
Nagaze <54482152+SrKirito@users.noreply.github.com>
2022-02-06 12:39:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+let win;
+
+const createWindow = () => {
+    win.setMenu(null)  
+    win.loadFile('index.html');
+};
+
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+    console.log("Everything is looking fine!");
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        win.webContents.send('video:metadata', metadata.format.duration);
+    });
+});
